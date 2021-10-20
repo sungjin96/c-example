8d6c7b01d1db60122d10c35cc762e00602a2d877
JJangseong <sungjin5891@gmail.com>
2021-10-20 20:41:08 +0900

    팩토리오

--- basic/main.cpp
+++ basic/main.cpp
@@ -1,12 +1,24 @@
 ﻿#include <stdio.h>
 
-int add(int a, int b) {
+int add(int a, int b)
+{
 	return a + b;
 }
 
+
+
+// i = 4
+// 1 * 2 * 3 * 4
 int main()
 {
-	printf("aaaaa");
+	int i = 4;
+	int result = 1;
+
+	for (int j = 0; j < i - 1; ++j)
+	{
+		result *= (j + 2);
+		printf("Result : %d\n", result);
+	}
 
 	return 0;
 }
