fac8c06d0f7d142d091ddab7237d6cc505b9fcf4
JJangseong <sungjin5891@gmail.com>
2021-10-21 20:48:09 +0900

    move factorial to function

--- basic/main.cpp
+++ basic/main.cpp
@@ -1,24 +1,24 @@
 ﻿#include <stdio.h>
 
-int add(int a, int b)
-{
-	return a + b;
-}
 
+int Factorial(int _iNum)
+{
+	int result = 1;
+
+	for (int j = 0; j < _iNum - 1; ++j)
+	{
+		result *= (j + 2);
+	}
+	return result;
+}
 
 
 // i = 4
 // 1 * 2 * 3 * 4
 int main()
 {
-	int i = 4;
-	int result = 1;
-
-	for (int j = 0; j < i - 1; ++j)
-	{
-		result *= (j + 2);
-		printf("Result : %d\n", result);
-	}
+	int result = Factorial(4);
+	printf("Result : %d\n", result);
 
 	return 0;
 }
