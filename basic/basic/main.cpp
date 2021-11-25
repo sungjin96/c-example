25e5d536e180abc306a1cd7209e13be49f8f4574
JJangseong <sungjin5891@gmail.com>
2021-11-25 21:34:16 +0900

    const

--- basic/basic/main.cpp
+++ basic/basic/main.cpp
@@ -1,36 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void Test(int* a)
+void Output(const int* PI)
 {
-	*a = 500;
-}
 
+}
 
 int main()
 {
-	const int i = 100;
-
-	int a = 0;
-	int* pInt = &a;
-
-	*pInt = 1;
-	pInt = nullptr;
-
-	// const 포인터
-	int b = 0;
-	const int* pConstInt = &a;
-
-	//*pConstInt = 100;
-	pConstInt = &b;
-
-	int* const pIntConst = &a;
-	*pIntConst = 400;
-	//pIntConst = &b;
-
-	// 초기화 시 가리킨 대상만 가리킴, 가리키는 원본을 수정 할 수 없음
-	const int* const pConstINtConst = nullptr;
-
+	int a = 100;
+	Output(&a);
 
 	return 0;
 }
