7ee9c0e6d6f762ee7a7149a39aa573a21584017f
JJangseong <sungjin5891@gmail.com>
2021-12-16 22:06:48 +0900

    Calculate

--- DataStructure/DataStructure/Stack.c
+++ DataStructure/DataStructure/Stack.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define INF 9999999
+#include <string.h>
 
-typedef struct {
-	int data;
+typedef struct Node{
+	char data[100];
 	struct Node* next;
 }Node;
 
@@ -13,17 +13,28 @@ typedef struct {
 
 Stack* stack;
 
-void push(int data) {
+void push(char data) {
 	Node* node = (Node*)malloc(sizeof(Node));
-	node->data = data;
+	strcpy(node->data, data);
 	node->next = stack->top;
 	stack->top = node;
 
 }
 
-int pop() {
+char* getTop() {
+	Node* top = stack->top;
+	return top->data;
+}
+
+char* pop() {
+	if (stack->top == NULL) {
+		printf("스택 언더플로우가 발생했습니다.\n");
+		return NULL;
+	}
+
 	Node* node = stack->top;
-	int data = node->data;
+	char* data = (char*)malloc(sizeof(char) * 100);
+	strcpy(data, node->data);
 	stack->top = node->next;
 	free(node);
 	return data;
@@ -32,14 +43,93 @@ int pop() {
 void show() {
 	Node* cur = stack->top;
 	while (cur != NULL) {
-		printf("%d \n", cur->data);
+		printf("%s \n", cur->data);
 		cur = cur->next;
 	}
 }
 
+int getPriority(char* i) {
+	if (!strcmp(i, "{")) return 0;
+	if (!strcmp(i, "+") || !strcmp(i, "-")) return 1;
+	if (!strcmp(i, "*") || !strcmp(i, "/")) return 2;
+	return 3;
+}
+
+char* transition(char** s, int size) {
+	char res[1000] = "";
+	for (int i = 0; i < size; i++) {
+		if (!strcmp(s[i], "+") || !strcmp(s[i], "-") || !strcmp(s[i], "*") || !strcmp(s[i], "/")) {
+			while (stack->top != NULL && getPriority(getTop()) >= getPriority(s[i])) {
+				strcat(res, pop()); strcat(res, " ");
+			}
+			push(s[i]);
+		}
+		else if(!strcmp(s[i], "(")) push(s[i]);
+		else if (!strcmp(s[i], ")")) {
+			while (strcmp(getTop(), "()")) {
+				strcat(res, pop()); strcat(res, " ");
+			}
+			pop();
+		}
+		else { strcat(res, s[i]); strcat(res, " "); }
+	}
+	while (stack->top != NULL) {
+		strcat(res, pop()); strcat(res, " ");
+	}
+	return res;
+}
+
+void calculate(char** s, int size) {
+	int x, y, z;
+	for (int i = 0; i < size; i++) {
+		if (!strcmp(s[i], "+") || !strcmp(s[i], "-") || !strcmp(s[i], "*") || !strcmp(s[i], "/")) {
+			x = atoi(pop());
+			y = atoi(pop());
+			if (!strcmp(s[i], "+")) z = y + x;
+			if (!strcmp(s[i], "-")) z = y - x;
+			if (!strcmp(s[i], "*")) z = y * x;
+			if (!strcmp(s[i], "/")) z = y / x;
+			char buffer[100];
+			sprintf(buffer, "%d", z);
+			push(stack, buffer);
+		}
+		else {
+			push(stack, s[i]);
+		}
+	}
+	printf("$s\n", pop());
+}
+
 int main() {
-	Stack stack;
-	stack.top = NULL;
+	stack = (Stack*)malloc(sizeof(Stack));
+	stack->top = NULL;
+	char a[100] = "( ( 3 + 4 ) * 5 ) - 5 * 7 * 5 - 5 * 10";
+	int size = 1;
+	for (int i = 0; i < strlen(a); i++) {
+		if (a[i] == ' ') size++;
+	}
+	char* ptr = strtok(a, " ");
+	char** input = (char**)malloc(sizeof(char*) * size);
+	for (int i = 0; i < size; i++) {
+		input[i] = (char*)malloc(sizeof(char) * 100);
+	}
+	for (int i = 0; i < size; i++) {
+		strcpy(input[i], ptr);
+		ptr = strtok(NULL, " ");
+	}
+	char b[1000] = "";
+	strcpy(b, transition(input, size));
+	printf("후위 표기법: %s\n", b);
+	size = 1;
+	for (int i = 0; i < strlen(b) - 1; i++) {
+		if (b[i] == ' ') size++;
+	}
+	char* ptr2 = strtok(b, " ");
+	for (int i = 0; i < size; i++) {
+		strcpy(input[i], ptr2);
+		ptr2 = strtok(NULL, " ");
+	}
+	calculate(input, size);
 
 	system("pause");
 	return 0;
