6a779000e05a5d23f914f4f1f6189bb05109b4dc
JJangseong <sungjin5891@gmail.com>
2021-12-17 22:02:18 +0900

    Add Queue

---
+++ DataStructure/DataStructure/Queue.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define INF 999999999
+
+typedef struct Node {
+	int data;
+	Node* next;
+}Node;
+
+typedef struct Queue {
+	Node* front;
+	Node* rear;
+	int count;
+} Queue;
+
+void push(Queue* queue, int data) {
+	Node* node = (Node*)malloc(sizeof(Node));
+	node->data = data;
+	node->next = NULL;
+	if (queue->count == 0) {
+		queue->front = node;
+	}
+	else {
+		queue->rear->next = node;
+	}
+	queue->rear = node;
+	queue->count++;
+}
+
+int pop(Queue* queue) {
+	if (queue->count == 0) {
+		printf("큐 언더플로우가 발생했습니다.\n");
+		return -INF;
+	}
+	Node* node = queue->front;
+	int data = node->data;
+	queue->front = node->next;
+	free(node);
+	queue->count--;
+	return data;
+}
+
+void show(Queue* queue) {
+	Node* cur = queue->front;
+	printf("--- 큐의 앞 ---\n");
+	while (cur != NULL) {
+		printf("%d\n ", cur->data);
+		cur = cur->next;
+	}
+	printf("--- 큐의 뒤 ---\n");
+}
+
+int main() {
+	Queue queue;
+	queue.front = queue.rear = NULL;
+	queue.count = 0;
+	push(&queue, 5);
+	push(&queue, 2);
+	push(&queue, 4);
+	show(&queue);
+	pop(&queue);
+	show(&queue);
+
+	return 0;
+}
