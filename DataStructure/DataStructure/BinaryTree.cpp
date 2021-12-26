527a207316eba563d527ba067910568c6edb1694
JJangseong <sungjin5891@gmail.com>
2021-12-26 21:56:56 +0900

    Add Binary Tree 01

---
+++ DataStructure/DataStructure/BinaryTree.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+	int data;
+	struct Node* leftChild;
+	struct Node* rightChild;
+} Node;
+
+Node* initNode(int data, Node* leftChild, Node* rightChild) {
+	Node* node = (Node*)malloc(sizeof(Node));
+	node->data = data;
+	node->leftChild = leftChild;
+	node->rightChild = rightChild;
+	return node;
+}
+
+int main() {
+	return 0;
+}
